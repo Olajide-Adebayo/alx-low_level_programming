0c012cb594ba5f67e4a99163f1f9ca4ce59aa040
Olajide-Adebayo <timothysure4@gmail.com>
2021-08-31 13:31:56 -0700

    <functions and nested loops>

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - entry point
+ * print Holberton
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char *c = "Holberton\n";
+
+while (*c)
+{
+_putchar(*c++);
+}
+return (0);
+}
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet - entry point
+ * print lowercase alphabet
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+int i = 'a';
+
+while (i <= 'z')
+{
+_putchar(i);
+i++;
+}
+_putchar('\n');
+}
