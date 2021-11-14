e780e903389a5d458d28d41a462d52c4dc33dcbe
Olajide-Adebayo <timothysure4@gmail.com>
2021-11-14 00:37:36 -0800

    <2-print_alphabet.c>

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * putchar 'a-z'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; c++)
+putchar(c);
+putchar('\n');
+return (0);
+}
