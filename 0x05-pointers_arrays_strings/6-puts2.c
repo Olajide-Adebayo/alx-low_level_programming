178f2c70c3e896e281725727562863ed7ac9a07d
Olajide-Adebayo <timothysure4@gmail.com>
2021-11-14 11:22:32 -0800

    <6-puts2.c>

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * puts2 - a funct that prints the first and every other character of a string.
+ * @str: declaration of str and paramters for the function puts2
+ * Return: Always 0.
+ */
+void puts2(char *str)
+{
+int c;
+char l;
+for (c = 0; str[c] != 0; c++)
+{
+if (c % 2 == 0)
+{
+l = str[c];
+_putchar(l);
+}
+}
+_putchar('\n');
+}
