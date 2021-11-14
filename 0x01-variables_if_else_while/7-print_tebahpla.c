22c380fc24fd6fdde9828929e6dd186fd4b175b3
Olajide-Adebayo <timothysure4@gmail.com>
2021-11-14 01:04:31 -0800

    <7-print_tebahpla.c>

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * printing alphabet in reverse order
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char c = 'z';
+while (c >= 'a')
+{
+putchar(c);
+c--;
+}
+putchar('\n');
+return (0);
+}
